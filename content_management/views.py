838b76ec18103569825d602b83675943cb1c1b29
Olena Liubynetska <olena.liuby@gmail.com>
2024-01-11 19:38:32 +0000

    feat: Add TenderPagination

--- content_management/views.py
+++ content_management/views.py
@@ -1,13 +1,20 @@
 from rest_framework import mixins
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 
 from content_management.models import Tender
 from content_management.serializers import TenderSerializer
 
 
+class TenderPagination(PageNumberPagination):
+    page_size = 9
+    max_page_size = 100
+
+
 class TenderViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
     queryset = Tender.objects.all()
     serializer_class = TenderSerializer
+    pagination_class = TenderPagination
 
     def get_queryset(self):
         queryset = super().get_queryset()
