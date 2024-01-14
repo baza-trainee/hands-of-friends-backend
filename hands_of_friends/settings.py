46fad0703e9c92750cbe82c371324f77a9346819
Olena Liubynetska <olena.liuby@gmail.com>
2024-01-14 15:59:54 +0000

    feat: add Render hostname to ALLOWED_HOSTS if provided

--- hands_of_friends/settings.py
+++ hands_of_friends/settings.py
@@ -31,6 +31,11 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") == "True"
 
 ALLOWED_HOSTS = []
 
+# Adding Render hostname to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
