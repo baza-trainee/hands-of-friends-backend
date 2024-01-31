25383067daee18f2055ef5ad2c21536968dfb73f
Olena Liubynetska <olena.liuby@gmail.com>
2024-01-31 12:05:36 +0000

    fix: Add permission to delete feedbacks

--- feedback/admin.py
+++ feedback/admin.py
@@ -10,9 +10,6 @@ class FeedbackFormAdmin(admin.ModelAdmin):
     def has_add_permission(self, request) -> bool:
         return False
 
-    def has_delete_permission(self, request, obj=None) -> bool:
-        return False
-
     def has_change_permission(self, request, obj=None) -> bool:
         return False
 
