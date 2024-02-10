78dafd04006fe556eb1607964141648fb4c71297
Olena Liubynetska <olena.liuby@gmail.com>
2024-02-10 15:27:20 +0000

    feat: Add & compile content_management app fields & messages for uk language

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
+from django.utils.translation import gettext as _
 from modeltranslation.admin import TranslationAdmin
 
 from content_management.models import (
@@ -19,7 +20,7 @@ class ImageAdminMixin:
             return mark_safe(f'<img src="{obj.image.url}" width="150" height="auto" />')
         return "No Image"
 
-    image_tag.short_description = "Image Preview"
+    image_tag.short_description = _("Image Preview")
 
 
 @admin.register(Tender)
@@ -48,10 +49,7 @@ class TeamMemberAdmin(TranslationAdmin, ImageAdminMixin):
 
 @admin.register(PartnerLogo)
 class PartnerLogoAdmin(admin.ModelAdmin, ImageAdminMixin):
-    list_display = (
-        "company_name",
-        "image_tag",
-    )
+    list_display = ("company_name", "image_tag")
     search_fields = ("company_name",)
 
 
@@ -68,6 +66,7 @@ class ContactsAdmin(TranslationAdmin):
     """Contacts Admin with singleton pattern"""
 
     list_display = ("phone_number", "email", "youtube_link", "facebook_link", "address")
+
     group_fieldsets = True
 
     def has_add_permission(self, request, obj=None):
