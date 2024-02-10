889172e7edc30d946096693f73d461f8309791b0
Olena Liubynetska <olena.liuby@gmail.com>
2024-02-10 15:26:13 +0000

    feat: Add & compile validation messages for uk language

--- validators/pdf_validation.py
+++ validators/pdf_validation.py
@@ -2,6 +2,7 @@
 
 from django.core.exceptions import ValidationError
 from django.db.models.fields.files import FieldFile
+from django.utils.translation import gettext as _
 
 from validators.constants import MAX_FILE_SIZE
 from validators.file_utils import FileUtility
@@ -15,10 +16,12 @@ def is_pdf(file_name: str) -> bool:
 def validate_pdf_file(file_field: FieldFile) -> None:
     """Validate that a file is a PDF and its size is within the limit."""
     if not is_pdf(file_field.name):
-        raise ValidationError("Unsupported file extension. Allowed extension is pdf.")
+        raise ValidationError(
+            _("Unsupported file extension. Allowed extension is pdf.")
+        )
 
     file_size = FileUtility.get_file_size(file_field)
     if file_size > MAX_FILE_SIZE:
         raise ValidationError(
-            f"PDF size is too large. Max size is {MAX_FILE_SIZE / 1024 / 1024} MB."
+            _(f"PDF size is too large. Max size is {MAX_FILE_SIZE / 1024 / 1024} MB.")
         )
